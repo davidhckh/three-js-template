de0b0312bfe6378a1609f46e5ed5e567f99da99a
davidhckh <david.heckhoff@gmail.com>
2022-09-01 20:12:57 +0200

    style: clean up

--- sources/Experience/Camera.js
+++ sources/Experience/Camera.js
@@ -1,7 +1,6 @@
 import * as THREE from "three";
 import Experience from "./Experience.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { MathUtils } from "three";
 
 export default class Camera {
   constructor() {
